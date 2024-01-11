45680a073613657592ac5c3400440166b229346a
MrWhip <gleb.kovalev.02@mail.ru>
2024-01-11 06:07:34 +0300

    Update config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/scloud-internship-to-do-list-vue3/',
   plugins: [
     vue(),
     vueJsx(),
